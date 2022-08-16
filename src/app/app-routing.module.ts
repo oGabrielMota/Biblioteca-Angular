e538c984153021eb305afca2ae08f098953a25b2
Gabriel Mota <gabriel.mota@gpnet.com.br>
2022-08-16 11:54:59 -0300

    Criação menu inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutoresComponent } from './autores/autores.component';
+import { HomeComponent } from './home/home.component';
+import { LivrosComponent } from './livros/livros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [],
+  },
+  {
+    path: 'livros',
+    component: LivrosComponent,
+    canActivate: [],
+  },
+  {
+    path: 'autores',
+    component: AutoresComponent,
+    canActivate: [],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LivrosComponent } from './livros/livros.component';
+import { AutoresComponent } from './autores/autores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LivrosComponent,
+    AutoresComponent
   ],
   imports: [
     BrowserModule,
